227ea9e04abe85a118cb92a998fa4ad2eb79e8a7
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-03-17 12:29:36 +0100

    feat: add spider

--- src/biomassml/pipeline.py
+++ src/biomassml/pipeline.py
@@ -1,4 +1,18 @@
 from .build_model import ARD_WRAPPERS, NO_ARD_WRAPPERS, build_coregionalized_model, build_model
+from .metrics import loocv_train_test
 
 
-def loocv_pipe(kernel, coregionalized: bool = False, ard: bool = False)
+def loocv_pipe(kernel, X, y, coregionalized: bool = False, ard: bool = False):
+    """
+    Perform leave-one-out cross-validation on the given kernel.
+    """
+    if ard:
+        if coregionalized:
+            model = build_coregionalized_model(X, y, kernel=ARD_WRAPPERS[kernel](X))
+        else:
+            model = build_model(X, y, kernel=ARD_WRAPPERS[kernel](X))
+    else:
+        if coregionalized:
+            model = build_coregionalized_model(X, y, kernel=NO_ARD_WRAPPERS[kernel](X))
+        else:
+            model = build_model(X, y, kernel=NO_ARD_WRAPPERS[kernel](X))
